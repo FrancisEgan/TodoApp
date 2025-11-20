7a44e8f61e3a6a115decf03afd8694ce23bc605c
Francis Egan <fegan@facilisgroup.com>
2025-11-20 09:42:55 -0500

    Remove scaffold unit test file

--- TodoApi.Tests/UnitTest1.cs
+++
@@ -1,5 +0,0 @@
-﻿// This file was the default template and has been replaced with comprehensive test files:
-// - Services/TokenServiceTests.cs
-// - Services/TodoCacheServiceTests.cs
-// - Endpoints/TodoEndpointsTests.cs
-// - Endpoints/AuthEndpointsTests.cs
